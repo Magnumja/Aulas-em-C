e153f89bb70e3efa4540896c850c37d5b18c955a
Magnumja <162545876+Magnumja@users.noreply.github.com>
2024-09-10 19:29:25 -0400

    Create aula5.c

---
+++ aulasfacul/aula5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define 
+
+int main() 
+{
+  int cont; 
+  //variavel para contar
+  cont = 0;
+  //estrutura de repeticao
+  while (cont < MAX)
+{
+    printf("Valot do contador: %d\n", cont);
+}
+return 0;
+}
