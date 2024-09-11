3c0d780fa2253eeba0472ad729a2dfc0fa7fdf41
Magnumja <162545876+Magnumja@users.noreply.github.com>
2024-09-11 16:36:32 -0400

    Update Aula5part2.c

--- aulasfacul/Aula5part2.c
+++ aulasfacul/Aula5part2.c
@@ -4,22 +4,26 @@
 
 int main ()
 {
-float n1,n2,med;
-int cont;
-while (cont<MAX)
-{
-  printf("Nota 1: ");
-  scanf("%f", &n1);
-  printf("%f", &n2);
-  scanf("%f", &n2);
-  med=(n1+n2)/2;
-//classificar
-
-if(med>7)
-  printf("Aprovado!\n");
-else
-  printf("Esude mais!\n);
-cont++;
-}
-return 0;
+    float n1, n2, med;
+    int cont = 0; // Inicializando 'cont'
+    
+    while (cont < MAX)
+    {
+        printf("Nota 1: ");
+        scanf("%f", &n1);
+        
+        printf("Nota 2: ");
+        scanf("%f", &n2);
+        
+        med = (n1 + n2) / 2;
+        
+        if (med > 7)
+            printf("Aprovado!\n");
+        else
+            printf("Estude mais!\n");
+        
+        cont++; // Incrementando o contador
+    }
+    
+    return 0;
 }
