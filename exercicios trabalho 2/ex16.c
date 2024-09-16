4af37a1758834a48d1002788acf8a8ed76e718da
Magnumja <162545876+Magnumja@users.noreply.github.com>
2024-09-16 19:55:29 -0400

    uptades

---
+++ exercicios trabalho 2/ex16.c
@@ -0,0 +1,25 @@
+// Faça um algoritmo que calcule o número fatorial de um número.
+// Ex.: 5! = 1x2x3x4x5 = 120
+// Lembrando que 0! =  1.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int numero, num = 0, resultado, fatorial = 0;
+
+    printf("Me fale o numero que voce deseja calcular o fatorial: ");
+    scanf("%d", &numero);
+
+    while (fatorial == 1)
+    {
+        fatorial = x - num;
+        num -= 1;
+    }
+
+    resultado = x * fatorial++;
+
+    printf("O resultado é: %d", resultado);
+}
