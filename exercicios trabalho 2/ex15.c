6bdbef1b917927389e31d91c5a6dc4e44d2c78ac
Magnumja <162545876+Magnumja@users.noreply.github.com>
2024-09-16 21:57:13 -0400

    uptades

--- exercicios trabalho 2/ex15.c
+++ exercicios trabalho 2/ex15.c
@@ -1,23 +1,37 @@
- //Escrever um algoritmo que leia 5 pares de valores (a,b), todos inteiros e positivos, um par de cada vez,
- // considere que o usuário sempre forneça “a” menor que “b”. Escreva os inteiros de a até b, incluindo a e b
+// Escrever um algoritmo que leia 5 pares de valores (a,b), todos inteiros e positivos, um par de cada vez,
+// considere que o usuário sempre forneça “a” menor que “b”. Escreva os inteiros de a até b, incluindo a e b.
 
+#include <stdio.h>
 
-
-int main() 
+int main()
 {
     int a, b;
     int x = 0;
 
-    while (x<5)
+    system("cls");
+
+    while (x < 5)
     {
-        printf("Escreva o par que deseja %d")
+        printf("Escreva o par que deseja (par %d): ", x + 1);
+        scanf("%d %d", &a, &b);
+
+        if (a < b)
+        {
+            int y = a;
+
+            while (y <= b)
+            {
+                printf("%d ", y);
+                y++;
+            }
+            printf("\n");
+            x++;
+        }
+        else
+        {
+            printf("Não permitido, 'a' deve ser menor que 'b'.\n");
+        }
     }
-    
-    
 
-
-
-
-
+    return 0;
 }
-
