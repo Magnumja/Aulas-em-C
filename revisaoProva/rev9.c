37213c414cfdd1a8796a0de5dfde0f3504c2aa04
Magnumja <162545876+Magnumja@users.noreply.github.com>
2024-10-05 17:42:47 -0400

    uptades

--- revisaoProva/rev9.c
+++ revisaoProva/rev9.c
@@ -1,48 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 int main()
 {
-    int senha, tentativa;
+    int senha, tentativa, num;
     system("cls");
 
-    printf("Digite a senha que deseja: (devera ter 4 digitos)");
-    scanf("%d", &senha);
+    printf("--------------------------- \n");
+    printf("       Seja Bem-vindo       \n");
+    printf("      Defina sua senha      \n");
+    printf(" Pressione 1 para continuar \n");
+    printf("        0 para parar        \n");
+    printf("--------------------------- \n");
+    scanf("%d", &num);
+    fflush(stdin);
+    system("cls");
 
-    while (senha < 1000 || senha > 9999)
+    if (num == 1) 
     {
-        printf("Deve ter 4 digitos, tente novamente! \n");
+        printf("Entendido! Iniciando o Jogo");
+        for (int i = 0; i < 3; i++) {
+            Sleep(500);
+            printf(".");
+        }
+        Sleep(500);
+        system("cls");
+
+        printf("Digite a senha que deseja (deve ter 4 dígitos):\n");
         scanf("%d", &senha);
-    }
+        fflush(stdin);
 
-    system("cls");
-
-    printf("Certo, agora confirme sua senha, voce tera 3 tentativas: ");
-    scanf("%d", &tentativa);
-
-    if (tentativa == senha)
-    {
-        printf("Tudo certo, sua senha foi salva e é %d .", senha);
-    }
-    else
-    {
-        int chute = 1;
-        while (chute < 3)
+        
+        while (senha < 1000 || senha > 9999)
         {
-            printf("Sua senha nao esta certa, tente novamente. \n");
-            scanf("%d", &tentativa);
-            chute++;
-            if (tentativa == senha)
-            {
-                printf("Tudo certo, sua senha foi salva e é %d .", senha);
-                break;
-            }
+            printf("A senha deve ter 4 dígitos, tente novamente!\n");
+            scanf("%d", &senha);
+            fflush(stdin);
+        }
 
-            if (chute == 3)
+        system("cls");
+
+        printf("Certo, agora confirme sua senha. Você terá 3 tentativas:\n");
+        int tentativas_restantes = 3;
+
+        for (int tentativa_atual = 1; tentativa_atual <= tentativas_restantes; tentativa_atual++) 
+        {
+            printf("Tentativa %d: ", tentativa_atual);
+            scanf("%d", &tentativa);
+            fflush(stdin);
+
+            if (tentativa == senha) 
             {
-                printf("Sua conta foi bloqueada.");
+                printf("Tudo certo, sua senha foi salva e é %d.\n", senha);
+                break;
+            } 
+            else if (tentativa_atual < tentativas_restantes) 
+            {
+                printf("Senha incorreta. Tente novamente.\n");
+            } 
+            else 
+            {
+                printf("Sua conta foi bloqueada.\n");
             }
         }
+    }
+    else if (num == 0) 
+    {
+        printf("Programa encerrado.\n");
     }
 
     return 0;
