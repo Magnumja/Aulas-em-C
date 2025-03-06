adeb7f5c57713a34e5c104e23beaf21371b3758d
Magnumja <162545876+Magnumja@users.noreply.github.com>
2025-03-06 03:22:18 -0400

    up

---
+++ trabalho 1 C++/ex1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+bool validarNota(float nota)
+{
+    return (nota >= 0.0 && nota <= 10.0);
+}
+
+int main()
+{
+
+    float nota;
+
+    do
+    {
+        cout << "Digite a nota do aluno (0 a 10): ";
+        cin >> nota;
+
+        if (!validarNota(nota))
+        {
+            cout << "Nota invalida! tente novamente.\n";
+        }
+    } 
+    
+    while (!validarNota(nota));
+    
+    cout << "Nota aceita: " << nota << endl;
+
+
+    return 0;
+}
